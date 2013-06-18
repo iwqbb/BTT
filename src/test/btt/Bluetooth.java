39e8ad299edb785f59bb4a0a710758c780ad3019
iwasaki <iwasaki@iwsk-dev>
2013-06-18 09:54:05 +0900

    Add bluetooth members and thier getter

--- src/test/btt/Bluetooth.java
+++ src/test/btt/Bluetooth.java
@@ -1,5 +1,35 @@
 package test.btt;
 
-public class Bluetooth {
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 
+public class Bluetooth {
+	private BluetoothAdapter mBTAdapter = null;
+	private BluetoothDevice mBTDevice = null;
+	private BluetoothSocket mBTSocket = null;
+	
+	/**
+	 * BluetoothAdapterを取得
+	 * @return BluetoothAdapter
+	 */
+	public BluetoothAdapter getBluetoothAdapter(){
+		return mBTAdapter;
+	}
+	
+	/**
+	 * BluetoothDeviceを取得
+	 * @return BluetoothDevice
+	 */
+	public BluetoothDevice getBluetoothDevice(){
+		return mBTDevice;
+	}
+	
+	/**
+	 * BluetoothSocketを取得
+	 * @return BluetoothSocket
+	 */
+	public BluetoothSocket getBluetoothSocket(){
+		return mBTSocket;
+	}
 }
