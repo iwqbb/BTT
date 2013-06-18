b66ce1119084a44a77ffe88136340bbd83c3da77
iwasaki <iwasaki@iwsk-dev>
2013-06-18 11:20:56 +0900

    Implement connectSocket method

--- src/test/btt/Bluetooth.java
+++ src/test/btt/Bluetooth.java
@@ -1,13 +1,19 @@
 package test.btt;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.util.Log;
 
 public class Bluetooth {
 	private BluetoothAdapter mBTAdapter = null;
 	private BluetoothDevice mBTDevice = null;
 	private BluetoothSocket mBTSocket = null;
+	
+	static public UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 	
 	/**
 	 * BluetoothAdapterを取得
@@ -31,5 +37,39 @@ public class Bluetooth {
 	 */
 	public BluetoothSocket getBluetoothSocket(){
 		return mBTSocket;
+	}
+	
+	/**
+	 * BluetoothSocketを生成して接続する
+	 * @return 新たに接続した場合にはtrue，そうでなければfalse
+	 * @throws IOException, NullPointerException
+	 */
+	public boolean connectSocket(){
+		if (mBTSocket != null && mBTSocket.isConnected()){
+			return false;
+		}
+		
+		try {
+			// Socket生成
+			mBTSocket = mBTDevice.createRfcommSocketToServiceRecord(MY_UUID);
+		} catch (NullPointerException e1) {
+			Log.e("Bluetooth", "通信先のデバイスが指定されていません");
+		} catch (IOException e2){
+			Log.e("Bluetooth", "Socketを生成できません");
+		}
+
+		try {
+			// Socket接続
+			mBTSocket.connect();
+		} catch (IOException e1) {
+			Log.e("Bluetooth", "Socketが通信先のデバイスに接続できません");
+			try {
+				mBTSocket.close();
+			} catch (IOException e2) {
+				Log.e("Bluetooth", "Socketを閉じるのに失敗しました");
+			}
+		}
+		
+		return true;
 	}
 }
