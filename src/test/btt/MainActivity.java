65066ad55bfb76904d4e9120cbddd4cc3ff4eadd
iwasaki <iwasaki@iwsk-dev>
2013-06-19 15:46:25 +0900

    Implement initialization and end processing of bluetooth on activity

--- src/test/btt/MainActivity.java
+++ src/test/btt/MainActivity.java
@@ -1,5 +1,7 @@
 package test.btt;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,12 +9,20 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private Bluetooth mBt;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Toast.makeText(this, "hoge...", Toast.LENGTH_LONG).show();
+		mBt = new Bluetooth();
+		try {
+			mBt.setBluetoothDevice(getString(R.string.BTDEVICE_NAME_BLUETOOTHMATE), getString(R.string.BTDEVICE_ADDRESS_BLUETOOTHMATE));
+			mBt.connectSocket();
+		} catch (Exception e) {
+			Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+		}
 	}
 
 	@Override
@@ -21,6 +31,16 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	public void onDestroy(){
+		try {
+			mBt.closeSocket();
+		} catch (IOException e) {
+			Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+		}
+	}
+	
     public void onClickButton1(View view){
     }
 }
